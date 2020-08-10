f35be5220ebd78612ae5e3c1bb0533e7ca0f4b5a
Christer Lingard <lingard.christer@gmail.com>
2020-08-10 10:45:17 +0200

    updates

--- zsh/.zshrc.sh
+++ zsh/.zshrc.sh
@@ -429,3 +429,11 @@ export LANG=en_US.UTF-8
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export GEM_HOME="$HOME/.gem"
+
+. /Users/christer/torch/install/bin/torch-activate
